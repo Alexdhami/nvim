5d80a7fca3e59f5abf6597cd958854cd285b8d39
AlexDhami <alexdhami864@gmail.com>
2025-07-05 01:55:30 +0545

    added some plugins

--- init.lua
+++ init.lua
@@ -6,3 +6,4 @@ require("core.options")         -- Basic Neovim settings
 require("core.keymaps")         -- Keybindings
 require("core.colorscheme")     -- Theme setup
 require("core.lazy")            -- Plugin definitions & lazy loading
+
