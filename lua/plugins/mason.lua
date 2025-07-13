ff4436948341755aa43e6e9b2b6d49ba14732aae
AlexDhami <alexdhami864@gmail.com>
2025-07-13 21:09:12 +0545

    updated auto install of pyright

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,13 @@
 -- Mason plugin for managing LSP installations
 
-require("mason").setup()
-require("mason-lspconfig").setup()
+-- Load the required modules first
+local mason = require("mason")
+local mason_lspconfig = require("mason-lspconfig")
+
+-- Setup mason
+mason.setup()
+
+-- Setup mason-lspconfig and ensure specific LSPs are installed
+mason_lspconfig.setup({
+  ensure_installed = { "pyright", "lua_ls" },
+})
