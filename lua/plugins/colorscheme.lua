e7a0d39cab88b46cd38169d1d681c09904dd30f2
alexdhami <alexdhami864@gmail.com>
2025-07-25 17:39:41 +0545

    added some themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,32 @@
--- plugins/tokyonight.lua
+-- plugins/colorscheme.lua
 
 return {
-  "folke/tokyonight.nvim",
-  config = function()
-    vim.cmd("colorscheme tokyonight") -- Set the colorscheme
-  end,
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,                             -- load immediately
+    priority = 1000,                          -- make sure it's loaded before others
+    config = function()
+      vim.cmd("colorscheme tokyonight-night") -- or "tokyonight", "storm", etc.
+    end,
+  },
+
+  -- Add other optional themes below ↓
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true, -- optional fallback or for switching later
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = true,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = true,
+  },
+  {
+    "navarasu/onedark.nvim",
+    lazy = true,
+  },
 }
