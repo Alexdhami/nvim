ce8970d3fcffd7924f82613509853b52ae291f0f
alexdhami <b2ok369@proton.me>
2025-11-14 22:09:35 +0545

    converted packer with lazy

---
+++ lua/Alex/lazy.lua
@@ -0,0 +1,72 @@
+return {
+
+    -- Themes
+    { "scottmckendry/cyberdream.nvim" },
+
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end,
+    },
+
+    -- Telescope
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+
+    -- Treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+    },
+
+    -- Harpoon
+    { "theprimeagen/harpoon" },
+
+    -- Undotree
+    { "mbbill/undotree" },
+
+    -- LSP Zero
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        dependencies = {
+            -- LSP
+            "neovim/nvim-lspconfig",
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+
+            -- Autocomplete
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-nvim-lua",
+            "saadparwaiz1/cmp_luasnip",
+
+            -- Snippets
+            "L3MON4D3/LuaSnip",
+            "rafamadriz/friendly-snippets",
+        },
+    },
+
+    -- Leap
+    {
+        "ggandor/leap.nvim",
+        config = function()
+            require("leap").setup({
+                labels = "sdjfkl",
+            })
+        end,
+    },
+
+    -- Mini pairs
+    {
+        "nvim-mini/mini.pairs",
+        config = function()
+            require("mini.pairs").setup()
+        end,
+    },
+}
