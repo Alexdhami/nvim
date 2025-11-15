45019221453fd640792eb0280e58127ae3bddf21
alexdhami <b2ok369@proton.me>
2025-11-15 09:49:33 +0545

    added tab next suggestion on CMP setup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -32,10 +32,30 @@ require("mason-lspconfig").setup({
 -- CMP setup
 local cmp = require("cmp")
 cmp.setup({
-    mapping = cmp.mapping.preset.insert({
+    mapping = {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif require("luasnip").expand_or_jumpable() then
+                require("luasnip").expand_or_jump()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif require("luasnip").jumpable(-1) then
+                require("luasnip").jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
         ["<C-Space>"] = cmp.mapping.complete(),
-    }),
+    },
     sources = {
         { name = "nvim_lsp" },
         { name = "buffer" },
@@ -50,4 +70,3 @@ cmp.setup({
 })
 
 lsp.setup()
-
