d4afa58db1aa2cba8bd311da27daa32e5b3b6715
alexdhami <b2ok369@proton.me>
2025-12-19 22:46:35 +0545

    lazy.nvim auto download if not found

--- init.lua
+++ init.lua
@@ -1,6 +1,18 @@
--- Add lazy.nvim to runtimepath manually
+-- Auto install lazy.nvim if not present
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath,
+    })
+end
+
+-- Add lazy.nvim to runtime path
 vim.opt.rtp:prepend(lazypath)
 
-
+-- Load main config
 require("Alex")
