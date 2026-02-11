7e50d4ebd518866dbe07f614c1ad375526f963d4
alexdhami <b2ok369@proton.me>
2026-02-11 18:52:17 +0545

    add 'MeanderingProgrammer/render-markdown.nvim' plugin

--- lua/Alex/lazy.lua
+++ lua/Alex/lazy.lua
@@ -5,8 +5,16 @@ return {
       config = function()
         require("Comment").setup()
       end
-    }
-,
+    },
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    },
     { 'brianhuster/live-preview.nvim' },
     -- Leap
     {
