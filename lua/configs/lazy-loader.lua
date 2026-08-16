a9fe17112c7718f77e7670ca4f4b7a883618a207
alexdhami <b2ok369@proton.me>
2026-08-16 17:34:48 +0545

    fix: spacing

--- lua/configs/lazy-loader.lua
+++ lua/configs/lazy-loader.lua
@@ -1,25 +1,25 @@
--- 1. Define where lazy.nvim should be saved on your filesystem
+--  Define where lazy.nvim should be saved on your filesystem
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
--- 2. Auto-install lazy.nvim if it doesn't exist
+--  Auto-install lazy.nvim if it doesn't exist
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ 
-    "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath 
-  })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ 
+        "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath 
+    })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 
--- 3. Add lazy.nvim to Neovim's runtime path so Lua can require it
+--  Add lazy.nvim to Neovim's runtime path so Lua can require it
 vim.opt.rtp:prepend(lazypath)
 
--- 4. Initialize lazy.nvim and load all specs from your plugins directory
+--  Initialize lazy.nvim and load all specs from your plugins directory
 require("lazy").setup("configs.plugins")
