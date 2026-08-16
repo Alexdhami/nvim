c46e80a16e0fa8a5e21e6e0c43692d405d14d1a3
alexdhami <b2ok369@proton.me>
2026-08-16 17:38:20 +0545

    fix: indentation and filepath

--- lua/configs/plugins/leap-nvim.lua
+++ lua/configs/plugins/leap-nvim.lua
@@ -1,17 +1,17 @@
--- Leap-nvim
+-- lua/plugins/leap-nvim.lua
 return {
-        url = "https://codeberg.org/andyg/leap.nvim",
-        name = "leap.nvim", -- optional but recommended
-        config = function()
-            require("leap").setup({
-                labels = "sdjfkl",
-            })
+    url = "https://codeberg.org/andyg/leap.nvim",
+    name = "leap.nvim", -- optional but recommended
+    config = function()
+        require("leap").setup({
+            labels = "sdjfkl",
+        })
 
-            vim.keymap.set(
-                { "n", "x", "o" },
-                "s",
-                "<Plug>(leap)",
-                { desc = "Leap Search (Window)" }
-            )
-        end,
+        vim.keymap.set(
+            { "n", "x", "o" },
+            "s",
+            "<Plug>(leap)",
+            { desc = "Leap Search (Window)" }
+        )
+    end,
 }
