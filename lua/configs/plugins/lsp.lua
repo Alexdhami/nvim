b17f3b1bc046513d75ceda23bb1cb5de6e01dad5
alexdhami <b2ok369@proton.me>
2026-08-17 13:29:45 +0545

    add: borders to hover

--- lua/configs/plugins/lsp.lua
+++ lua/configs/plugins/lsp.lua
@@ -33,10 +33,14 @@ return {
                 local opts = { buffer = bufnr }
 
                 vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
                 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
                 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
                 vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+
+                -- Explicitly force rounded border on hover
+                vim.keymap.set("n", "K",function ()  
+                    vim.lsp.buf.hover({border = "rounded"})
+                end, opts)
             end)
 
             -- Mason-LSP Setup (ensure_installed + handlers together)
