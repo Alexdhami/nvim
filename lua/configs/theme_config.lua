bf0c5f2814d2aa1ffcdc169e144962ff5470b175
alexdhami <b2ok369@proton.me>
2026-08-18 16:43:29 +0545

    costomize: hover window

--- lua/configs/theme_config.lua
+++ lua/configs/theme_config.lua
@@ -6,6 +6,12 @@ function ChangeTheme(color)
     vim.api.nvim_set_hl(0, "Pmenu", { bg = "#1e1e2e", fg = "#cdd6f4" })
     vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#fff3ff", fg = "#130a3a", bold = true })
     vim.api.nvim_set_hl(0, "FloatBorder", {fg = "#036666" })
+
+    -- Hover window content (text & background)
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e1e2e", fg = "#cdd6f4" })
+
+    -- Hover window border frame
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#1e1e2e", fg = "#89b4fa" })
 end
 
 ChangeTheme("cyberdream")
